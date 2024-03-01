2f3070be2d7bbfd0a7d25553fa240c5408e2a7e8
sjwestern <sjwestern@users.noreply.github.com>
2024-03-01 03:31:15 +0000

    swap to use microsft developed external git

--- .devcontainer/postCreateCommand_cloneAzDevOpsRepo.sh
+++ .devcontainer/postCreateCommand_cloneAzDevOpsRepo.sh
@@ -3,14 +3,27 @@ if [ -z "$1" ] then
     echo "repo url required"
     return 1
 fi
-if [ -z "$2" ] then 
+if [ -z "$3" ] then 
+    echo "clone folder"
+    return 1
+fi
+if [ -z "$3" ] then 
     echo "az PAT token required"
     return 1
 fi
 
 repo_url=$1
-az_pat=$2
+folder=$2
+az_pat=$3
+
 
 B64_PAT=$(printf ":%s" "${az_pat}" | base64) || true
-git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone "${repo_url}" || true
+if [ ! -d "$folder" ] ; then
+    git -c http.extraHeader="Authorization: Basic ${B64_PAT}" clone "${repo_url}" "$folder" || true
+else
+    cd "$folder"
+    git pull $URL
+    cd ..
+fi
+
 
